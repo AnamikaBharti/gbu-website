10c846b7a5f441b98a382629acaec82beba7c4f8
Ashwani Kushwaha <ashwanik346981@gmail.com>
2025-07-28 17:24:34 +0530

    update faculty

--- src/components/faculty/FacultyTabs.jsx
+++ src/components/faculty/FacultyTabs.jsx
@@ -1,47 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// const FacultyTabs = ({ tabItems, activeTab, onTabChange }) => {
-//   return (
-//     <section className="bg-blue-900 text-white">
-//       <div className="container mx-auto px-4">
-//         <div className="max-w-6xl mx-auto">
-//           <div className="flex flex-wrap">
-//             {tabItems.map((tab) => (
-//               <button
-//                 key={tab.id}
-//                 onClick={() => onTabChange(tab.id)}
-//                 className={`px-4 py-4 text-xs font-medium transition-colors border-b-2 ${
-//                   activeTab === tab.id
-//                     ? 'border-white border-[1px] border-solid bg-blue-800'
-//                     : 'border-transparent hover:bg-blue-800'
-//                 }`}
-//               >
-//                 {tab.label}
-//               </button>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// FacultyTabs.propTypes = {
-//   tabItems: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       label: PropTypes.string.isRequired,
-//     })
-//   ).isRequired,
-//   activeTab: PropTypes.string.isRequired,
-//   onTabChange: PropTypes.func.isRequired,
-// };
-
-// export default FacultyTabs;
-
-
-
 import React from 'react';
 import { 
   User, 
@@ -71,6 +27,7 @@ const tabs = [
   { id: 'publications', label: 'Publications', icon: FileText },
   { id: 'talks', label: 'Invited Talks', icon: Mic },
   { id: 'awards', label: 'Awards', icon: Trophy },
+  { id: 'other', label: 'Other', icon: Heart },
   
 ];
 
